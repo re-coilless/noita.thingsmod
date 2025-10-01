7a2f134da0d2e432af96470c3e06f73214ebb059
Evaisa <evagiacosa1@gmail.com>
2025-10-01 18:00:18 +0200

    better

--- init.lua
+++ init.lua
@@ -1,28 +1,32 @@
 local content_library = dofile_once("mods/noita.thingsmod/content.lua")
 
-local function register_callbacks()
-	for _, module_id in ipairs(content_library) do
-		local mod = dofile_once("mods/noita.thingsmod/content/" .. module_id .. "/module.lua")
-		if mod then
-			for callback_name, callback_func in pairs(mod) do
-				if type(callback_func) ~= "function" then
-					goto continue
-				end
+local callback_names = {
+  "OnBiomeConfigLoaded",
+  "OnCountSecrets",
+  "OnMagicNumbersAndWorldSeedInitialized",
+  "OnModInit",
+  "OnModPostInit",
+  "OnModPreInit",
+  "OnModSettingsChanged",
+  "OnPausePreUpdate",
+  "OnPausedChanged",
+  "OnPlayerDied",
+  "OnPlayerSpawned",
+  "OnWorldInitialized",
+  "OnWorldPostUpdate",
+  "OnWorldPreUpdate",
+}
 
-				local curr_global = _G[callback_name]
-				if type(curr_global) == "function" then
-					_G[callback_name] = function(...)
-						curr_global(...)
-						return callback_func(...)
-					end
-				else
-					_G[callback_name] = callback_func
+for _, callback_name in ipairs(callback_names) do
+	_G[callback_name] = function(...)
+		for _, module_id in ipairs(content_library) do
+			local mod = dofile_once("mods/noita.thingsmod/content/" .. module_id .. "/module.lua")
+			if mod and mod[callback_name] then
+				local success, error_msg = pcall(mod[callback_name], ...)
+				if not success then
+					print(mod.name .. " Error: " .. error_msg)
 				end
-
-				::continue::
 			end
 		end
-	end
+  	end
 end
-
-register_callbacks()
