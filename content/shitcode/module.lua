cab12e5b60b1de253873ab45c1031a8acc1bc95c
Evaisa <evagiacosa1@gmail.com>
2025-10-01 17:25:44 +0200

    shitcode module

---
+++ content/shitcode/module.lua
@@ -0,0 +1,57 @@
+return {
+	name = "Shitcode",
+	description = "Adds shit code",
+	callbacks = {
+		OnModPreInit = function () 
+
+		end,
+		OnModInit = function () 
+
+		end,
+
+		OnModPostInit = function () 
+
+		end,
+
+		OnPlayerSpawned = function ( player_entity ) 
+
+		end,
+
+		OnPlayerDied = function ( player_entity ) 
+
+		end,
+
+		OnWorldInitialized = function () 
+
+		end,
+
+		OnWorldPreUpdate = function () 
+
+		end,
+
+		OnWorldPostUpdate = function () 
+
+		end,
+
+		OnBiomeConfigLoaded = function () 
+
+		end,
+
+		OnMagicNumbersAndWorldSeedInitialized = function () 
+
+		end,
+
+		OnPausedChanged = function ( is_paused, is_inventory_pause ) 
+
+		end,
+
+		OnModSettingsChanged = function () 
+
+		end,
+
+		OnPausePreUpdate = function () 
+
+		end
+	
+	}
+}
