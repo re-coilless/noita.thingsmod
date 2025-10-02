9a657384ba1b59b5a227a21a5f1bd9116f931f4f
Recoilless Interactive <68288735+re-coilless@users.noreply.github.com>
2025-10-03 00:36:16 +0400

    testing

--- content/index_compendium/module.lua
+++ content/index_compendium/module.lua
@@ -3,6 +3,13 @@ return {
 	description = "A bunch of fancy HermeS Index stuff.",
 	authors = "Bruham",
 	
-	OnThingsCalled = function( modules ) print("test1") return true end,
-	OnWorldPreUpdate = function() print("test2") end,
+	-- OnThingsCalled = function( modules ) end,
+
+	--disclaimer that not all photos are accurate (some species are incredibly hard to source good pictures of in both forms)
+
+	--The Best Book of Cats (obtained by throwing good book of kats to the hiisi anvil)
+	--The Good Book of Kats (obtained by throwing good book of cats to the hiisi anvil)
+	--names, child/adult forms (all pics to 216 res), backstory
+	--pet counter that is stored in the settings
+	--page flipping sound and anim
 }
