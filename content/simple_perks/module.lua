96f0a9012be904151108b6924eddfc3b9f2d06c9
Copi <54731850+Ramiels@users.noreply.github.com>
2025-10-02 04:12:44 -0400

    Update module.lua

--- content/simple_perks/module.lua
+++ content/simple_perks/module.lua
@@ -2,10 +2,10 @@ local nxml = dofile_once("mods/Apotheosis/lib/nxml.lua")
 local module_filepath = "mods/noita.thingsmod/content/simple_perks/"
 
 return {
-	name = "Conga Stuff",
-	description = "Adds stuff",
-	authors = "Conga Lyne",
-	OnModPreInit = function () 
+	name = "Perks",
+	description = "Adds perks.",
+	authors = {"Conga Lyne", "Copi"},
+	OnModPreInit = function ()
 		for k=1,30 do
 			print("aaaaaaaaaaaaaaaaaaaaaaa")
 		end
@@ -26,61 +26,20 @@ return {
 		--Player Editor
 		do
 			local path = "data/entities/player_base.xml"
-				local xml = nxml.parse(ModTextFileGetContent(path))
-				xml:add_child(nxml.parse([[
-				<LuaComponent
-				script_damage_about_to_be_received="mods/noita.thingsmod/content/simple_perks/scripts/perks/take_damage.lua"
-				execute_every_n_frame="-1"
-				execute_times="-1"
-				remove_after_executed="0"
-				>
-				</LuaComponent>
-				]]))
+			local xml = nxml.parse(ModTextFileGetContent(path))
+			xml:add_child(nxml.parse([[
+			<LuaComponent
+			script_damage_about_to_be_received="mods/noita.thingsmod/content/simple_perks/scripts/perks/take_damage.lua"
+			execute_every_n_frame="-1"
+			execute_times="-1"
+			remove_after_executed="0"
+			>
+			</LuaComponent>
+			]]))
 			ModTextFileSetContent(path, tostring(xml))
 		end
+
+		-- Inject bountiful hunter power-ups
+		ModLuaFileAppend("data/scripts/items/drop_money.xml", "mods/noita.thingsmod/content/simple_perks/scripts/drop_booster.lua")
 	end,
-
-	OnModPostInit = function () 
-
-	end,
-
-	OnPlayerSpawned = function ( player_entity ) 
-
-	end,
-
-	OnPlayerDied = function ( player_entity ) 
-
-	end,
-
-	OnWorldInitialized = function () 
-
-	end,
-
-	OnWorldPreUpdate = function () 
-
-	end,
-
-	OnWorldPostUpdate = function () 
-
-	end,
-
-	OnBiomeConfigLoaded = function () 
-
-	end,
-
-	OnMagicNumbersAndWorldSeedInitialized = function () 
-		
-	end,
-
-	OnPausedChanged = function ( is_paused, is_inventory_pause ) 
-
-	end,
-
-	OnModSettingsChanged = function () 
-
-	end,
-
-	OnPausePreUpdate = function () 
-
-	end
 }
