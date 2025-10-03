9d6700aec34b7ec70d6e2cc11f088514c8ba1d2e
Nathan <nathan.titirangi@gmail.com>
2025-10-03 15:12:15 +1300

    move perks to use positional_insert

--- content/simple_perks/scripts/perks/custom_perks.lua
+++ content/simple_perks/scripts/perks/custom_perks.lua
@@ -1,4 +1,8 @@
-local custom_perk_perkappends = {
+local _require = require
+dofile("mods/noita.thingsmod/require.lua")
+local table_utils = require "lib.table_utils.table_utils"
+
+local custom_perks = {
 	{
 		id = "WET_ARMOR",
 		id_append = "STAINLESS_ARMOUR",
@@ -63,49 +67,18 @@ local custom_perk_perkappends = {
 	},
 }
 
-for _, v in ipairs(custom_perk_perkappends) do
+for _, v in ipairs(custom_perks) do
 	local id_lower = v.id:lower()
 	local prefix = "noita_thingsmod_simple_perks_"
 	v.ui_name = "$" .. prefix .. "perkname_" ..id_lower
 	v.ui_description = "$" .. prefix .. "perkdesc_" ..id_lower
 	v.ui_icon = ("mods/noita.thingsmod/content/simple_perks/ui_gfx/perk_icons/%s_ui.png"):format(id_lower)
 	v.perk_icon = ("mods/noita.thingsmod/content/simple_perks/items_gfx/perks/%s.png"):format(id_lower)
+	v.id = "NOITA_THINGSMOD_" .. v.id
+	v.author = v.author or "Every Things Dev Team"
+	v.mod = v.mod or "Every Things"
 end
 
 
-do
-	for k=1,#custom_perk_perkappends
-	do local v = custom_perk_perkappends[k]
-
-		v.id = "NOITA_THINGSMOD_" .. v.id
-
-		-- Disables the perk from spawning if it's not unlocked
-		if v.unlock_flag then
-			if HasFlagPersistent( v.unlock_flag ) == false then
-				v.not_in_default_perk_pool = true
-			end
-		end
-
-		--Adds Perk into the list at the position we want
-		if v.id_prepend == nil and v.id_append == nil then
-			v.author    = v.author  or "Mod Name Dev Team"
-			v.mod       = v.mod     or "Mod Name Mod"
-			table.insert(perk_list,v)
-		else
-			for z=1,#perk_list
-			do c = perk_list[z]
-				if c.id == v.id_prepend then
-					v.author    = v.author  or "Mod Name Dev Team"
-					v.mod       = v.mod     or "Mod Name Mod"
-					table.insert(perk_list,z,v)
-					break
-				elseif c.id == v.id_append or z == #perk_list then
-					v.author    = v.author  or "Mod Name Dev Team"
-					v.mod       = v.mod     or "Mod Name Mod"
-					table.insert(perk_list,z + 1,v)
-					break
-				end
-			end
-		end
-	end
-end
+table_utils.positional_insert(perk_list, custom_perks)
+require = _require
