86a7708f396b587496de197332c0440b729eb9b3
Nathan <nathan.titirangi@gmail.com>
2025-10-03 14:48:48 +1300

    seperate out append system

--- require.lua
+++ require.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: lowercase-global
 ---To use, make sure that your context does
 ---```lua
----dofile_once("mods/noita.thingsmod/require.lua")
+---dofile("mods/noita.thingsmod/require.lua")
 ---```
 ---at the start.
 function require(modname)
